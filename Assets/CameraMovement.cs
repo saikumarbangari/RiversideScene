835abab426ac010d5657f185c7784c88fa3a5408
Saikumar Bangari <65330729+saikumarbangari@users.noreply.github.com>
2020-08-13 20:12:42 +1000

    added camera movement

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+	public Vector3 center;
+
+	Vector3 v = Vector3.zero;
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+		v = v + 0.001f * (center - transform.position).normalized;
+		transform.position = transform.position + v;
+		transform.LookAt(transform.position + v);
+		MoveCenter();
+	}
+
+	void MoveCenter()
+	{
+		Vector2 rand = 5 * Random.insideUnitCircle;
+		Vector3 v3 = new Vector3(rand.x, 0, rand.y);
+		center = center + v3;
+	}
+}
